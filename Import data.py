8f8d2ee1f45455a18e790abc6cda6faf1f24a8ba
linhuang0 <120541669+linhuang0@users.noreply.github.com>
2023-06-29 14:22:21 +1200

    Add simple trading strategy
     Build fb simple trading strategy

--- Import data.py
+++ Import data.py
@@ -94,7 +94,33 @@ plt.legend()
 plt.show()
 
 
+#1. Munging the stock data and add two columns - MA10 and MA50
+#import FB's stock data, add two columns - MA10 and MA50
+#use dropna to remove any "Not a Number" data
+fb['MA10'] = fb['Close'].rolling(10).mean()
+fb['MA50'] = fb['Close'].rolling(50).mean()
+fb = fb.dropna()
+fb.head()
 
+#2. Add "Shares" column to make decisions base on the strategy
+#Add a new column "Shares", if MA10>MA50, denote as 1 (long one share of stock), otherwise, denote as 0 (do nothing)
+fb['Shares'] = [1 if fb.loc[ei, 'MA10']>fb.loc[ei, 'MA50'] else 0 for ei in fb.index]
+#Add a new column "Profit" using List Comprehension, for any rows in fb, if Shares=1, the profit is calculated as the close price of 
+#tomorrow - the close price of today. Otherwise the profit is 0.
 
+#Plot a graph to show the Profit/Loss
 
+fb['Close1'] = fb['Close'].shift(-1)
+fb['Profit'] = [fb.loc[ei, 'Close1'] - fb.loc[ei, 'Close'] if fb.loc[ei, 'Shares']==1 else 0 for ei in fb.index]
+fb['Profit'].plot()
+plt.axhline(y=0, color='red')
 
+#3. Use .cumsum() to display our model's performance if we follow the strategy
+#Use .cumsum() to calculate the accumulated wealth over the period
+
+fb['wealth'] = fb['Profit'].cumsum()
+fb.tail()
+#plot the wealth to show the growth of profit over the period
+
+fb['wealth'].plot()
+plt.title('Total money you win is {}'.format(fb.loc[fb.index[-2], 'wealth']))
